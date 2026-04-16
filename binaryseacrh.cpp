cdf62ebbcbc37d78db1ca709c29f8daea88bac5c
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-04-16 11:11:21 +0700

    menambahkan looping pada array

--- binaryseacrh.cpp
+++ binaryseacrh.cpp
@@ -73,6 +73,35 @@ void binarySearch()
     char ulang;
     do 
     {
+        cout << "\n===============================\n";
+        cout << "          Pencarian Binary Search        \n";
+        cout << "=================================\n";
+
+        cout << "Masukkan element yang ingin dicari: ";
+        cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do 
+        {
+            int mid = (low + high) /2;
+
+            if (element[mid] == x)
+            {
+                cout << "\n[] Eleme" << x << "ditemukan pada indeks " << mid << "\n";
+                return;
+            }
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
+        }while (low <= high);
         
+        if (low > high)
     }
 }
